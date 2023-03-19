eab43d76be3b287a3ce4ed61814a4a8eda743ea3
mijinnnnn <128116347+mijinnnnn@users.noreply.github.com>
2023-03-20 01:24:26 +0900

    Update 권미진_기둥과보.py

--- 03.20(월)/권미진_기둥과보.py
+++ 03.20(월)/권미진_기둥과보.py
@@ -1,1 +1,44 @@
+def solution(n, build_frame):
+    global answer
+    answer = []
+    for i in build_frame:
+        x = i[0]
+        y = i[1]
+        a = i[2]
+        b = i[3]
+        if b == 1:
+            build(x,y,a)
+        elif b == 0:
+            delete(x,y,a)
+    answer.sort()
+    return answer
 
+def build(x,y,a):
+    global answer
+    if a == 0 and (y == 0 or ([x,y,1] in answer) or ([x-1,y,1] in answer) or ([x,y-1,0] in answer)):
+        answer.append([x,y,a])
+    elif a == 1 and (([x,y-1,0] in answer) or ([x+1,y-1,0] in answer) or (([x-1,y,1] in answer) and ([x+1,y,1] in answer))):
+        answer.append([x,y,a])
+    else:
+        flag = False
+        
+def delete(x,y,a):
+    global answer,flag,temp,answer1
+    answer1 = answer.copy()
+    answer1.remove([x,y,a])
+    flag = True
+    temp = []
+    for i in answer1:
+        check(i[0],i[1],i[2])
+    if flag:
+        answer.remove([x,y,a])
+
+
+def check(x,y,a):
+    global flag,temp, answer1
+    if a == 0 and (y == 0 or ([x,y,1] in answer1) or ([x-1,y,1] in answer1) or ([x,y-1,0] in answer1)):
+        temp.append([x,y,a])
+    elif a == 1 and (([x,y-1,0] in answer1) or ([x+1,y-1,0] in answer1) or (([x-1,y,1] in answer1) and ([x+1,y,1] in answer1))):
+        temp.append([x,y,a])
+    else:
+        flag = False
