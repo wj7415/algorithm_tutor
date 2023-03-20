da9073be9462f4efdd34aafc76aca500d5d40a8b
DONGWANKOH <128116198+DONGWANKOH@users.noreply.github.com>
2023-03-20 12:47:15 +0900

    Update 고동완_기둥과보.py

--- 03.20(월)/고동완_기둥과보.py
+++ 03.20(월)/고동완_기둥과보.py
@@ -1,3 +1,5 @@
+#부분 점수 19.2점
+
 def solution(n, build_frame):
     global bar, beam
     bar = [[0 for i in range(n + 1)] for i in range(n + 1)]
@@ -46,3 +48,13 @@ def solution(n, build_frame):
                 ans.append([x,y,1])
 
     return ans
+
+# def main():
+#     n=int(input())
+#     # build_frame=[[input()]] #???이중 list 받기
+#     # build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
+#     build_frame=[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
+#     print(solution(n, build_frame))
+
+# if __name__=='__main__':
+#     main()
